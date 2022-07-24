6ded51bddd63ecefb0f43e5f960d202ed0e79ac4
nisan-cb <nnisan858@gmail.com>
2022-07-24 22:35:11 +0300

    log db commection status

--- src/server/postgres.ts
+++ src/server/postgres.ts
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
-console.log("connection url = ", process.env.DATABASE_URL);
 
 const pool = new Pool({
     connectionString:
@@ -10,8 +9,11 @@ const pool = new Pool({
         rejectUnauthorized: false,
     },
 });
+pool.connect()
+    .then(() => console.log('connected to remote DB'))
+    .catch((err: any) => console.log('unable connect to DB : ', err));
 
-pool.connect();
+
 
 export async function getPokeData(offset: number) {
     const queryText = `SELECT * FROM pokeDataTable ORDER BY id LIMIT 20 OFFSET ${offset}`;
