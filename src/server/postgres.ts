e43ad8e58dad505fb8c18acadf66fe0b360cf3b6
nisan-cb <nnisan858@gmail.com>
2022-07-24 22:51:02 +0300

    prevent sql injection

--- src/server/postgres.ts
+++ src/server/postgres.ts
@@ -16,27 +16,27 @@ pool.connect()
 
 
 export async function getPokeData(offset: number) {
-    const queryText = `SELECT * FROM pokeDataTable ORDER BY id LIMIT 20 OFFSET ${offset}`;
+    const queryText = `SELECT * FROM pokeDataTable ORDER BY id LIMIT 20 OFFSET $1`;
     try {
-        return await pool.query(queryText).then((res: any) => res.rows);
+        return await pool.query(queryText, [offset]).then((res: any) => res.rows);
     } catch (e) {
         console.error(e);
     }
 }
 
 export async function getDataForPokemonPage(id: number) {
-    const queryText = `SELECT * FROM pokeDataTable WHERE id=${id} LIMIT 1`;
+    const queryText = `SELECT * FROM pokeDataTable WHERE id=$1 LIMIT 1`;
     try {
-        return await pool.query(queryText).then((res: any) => res.rows);
+        return await pool.query(queryText, [id]).then((res: any) => res.rows);
     } catch (e) {
         console.error(e);
     }
 }
 
 export async function getPokemonsViaSearchBar(str: string, offset: number) {
-    const queryText = `SELECT * FROM pokeDataTable WHERE name~'${str}' LIMIT 20 OFFSET ${offset}`;
+    const queryText = `SELECT * FROM pokeDataTable WHERE name~$1 LIMIT 20 OFFSET $2`;
     try {
-        return await pool.query(queryText).then((res: any) => res.rows);
+        return await pool.query(queryText, [str, offset]).then((res: any) => res.rows);
     } catch (e) {
         console.error(e);
     }
